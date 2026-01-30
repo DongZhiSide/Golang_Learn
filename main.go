fc5e8dc3bbe56a3f8354249594aa8ab1c1c3810d
DongZhiSide <DongZhiSide@mydns.com>
2026-01-30 09:05:19 +0000

    修复：启动 syncmod。

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"golanglearn/syncmod"
 )
 
 func main() {
-	urls := []string{"a", "b", "c"}
-	for _, v := range urls {
-		go func() {
-			// 捕获了同一个 v（range 的循环变量会被复用）
-			fmt.Println(v)
-		}()
-	}
-	time.Sleep(100 * time.Millisecond)
+	syncmod.Rsm_main()
 }
