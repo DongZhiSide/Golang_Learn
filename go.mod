e271a9c8b78ca9287922ae17a6654988b3c0408a
DongZhiSide <DongZhiSide@mydns.com>
2026-02-25 05:40:28 +0000

    - 新增：新增 xorm.io/xorm 模块示例代码。

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module golanglearn
 
 go 1.26.0
+
+require (
+	github.com/go-sql-driver/mysql v1.8.1
+	xorm.io/xorm v1.3.11
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+	xorm.io/builder v0.3.13 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,49 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log/slog"
+	"os"
 
+	_ "github.com/go-sql-driver/mysql"
+	"xorm.io/xorm"
+)
+
+type MysqlSettings struct {
+	Username, Password string
+	Ip, Port           string
+	Database           string
+}
+
+var global MysqlSettings
+
+func main() {
+	err := read()
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+
+	auth := global.Username + ":" + global.Password
+	addr := global.Ip + ":" + global.Port
+	database := global.Database
+	dsn := fmt.Sprintf("%s@tcp(%s)/%s?charset=utf8", auth, addr, database)
+
+	e, err := xorm.NewEngine("mysql", dsn)
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+	f, err := os.Create("backup.sql")
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+	defer f.Close()
+
+	err = e.DumpAll(f)
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
 }
