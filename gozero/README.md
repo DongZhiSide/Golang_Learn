f05327fe35d0e07937104a6e5ffa9b2d68e27128
DongZhiSide <mc_icy14@outlook.com>
2026-03-20 16:28:07 +0800

    ✨ feat(gozero):  添加不完整示例代码

---
+++ gozero/README.md
@@ -0,0 +1,7 @@
+# go-zero
+
+
+
+## go-zero 的参考资料
+
+1. [go-zero 指南.](https://go-zero.dev/zh-cn/guides)
---
+++ gozero/user/api/internal/handler/deleteuserhandler.go
@@ -0,0 +1,31 @@
+// Code scaffolded by goctl. Safe to edit.
+// goctl 1.9.2
+
+package handler
+
+import (
+	"net/http"
+
+	"api/api/internal/logic"
+	"api/api/internal/svc"
+	"api/api/internal/types"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func DeleteUserHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.DeleteUserRequest
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := logic.NewDeleteUserLogic(r.Context(), svcCtx)
+		resp, err := l.DeleteUser(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
